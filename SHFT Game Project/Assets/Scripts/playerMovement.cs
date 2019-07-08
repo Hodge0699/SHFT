8b03af9a96a2940b7bb81a2441fb47400323b76f
Hodge0699 <39035064+Hodge0699@users.noreply.github.com>
2019-07-08 18:49:44 +0100

    Player Movement
    Player moves with WASD keys all directions

---
+++ SHFT Game Project/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float PlayerSpeed = 10;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+     void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+     void Update()
+    {   // lets the player move in any direction
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        //Keeps the player movement speed at the correct speed when moving at an angle
+        moveDirection = moveDirection.normalized * PlayerSpeed;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
+      
