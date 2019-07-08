b6cf2f5b604482f9da88b8c632318fe238451a62
Hodge0699 <39035064+Hodge0699@users.noreply.github.com>
2019-07-08 19:12:46 +0100

    Camera
    Camera follows player
    
    this is a simple camera to be improved

---
+++ SHFT Game Project/Assets/Scripts/CameraPlayerFollow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerFollow : MonoBehaviour
+{
+   
+
+
+    public Transform target;
+    public Vector3 offset;
+    public bool userOffsetValues;
+    public float rotateSpeed;
+    public Transform pivot;
+
+    void Start()
+    {
+        if (!userOffsetValues)
+        {
+            offset = target.position - transform.position;
+
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        
+
+    }
+
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(vertical, 0, 0);
+
+        if (pivot.rotation.eulerAngles.x > 45f && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(45f, 0, 0);
+        }
+        if (pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 315f)
+        {
+            pivot.rotation = Quaternion.Euler(315f, 0, 0);
+        }
+
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+        }
+        transform.LookAt(target);
+    }
+}
