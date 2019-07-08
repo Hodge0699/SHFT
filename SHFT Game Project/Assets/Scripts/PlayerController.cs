c494f7fc2aee1e51e0e21c60e46c30bcd5b7f90e
Hodge0699 <39035064+Hodge0699@users.noreply.github.com>
2019-07-08 19:45:06 +0100

    player stats
    player stats added
    changed for balancing later

--- SHFT Game Project/Assets/Scripts/PlayerController.cs
+++ SHFT Game Project/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
-{
+{  // Player movement  Varibles 
     public float PlayerSpeed = 10;
     public CharacterController controller;
     private Vector3 moveDirection;
+    // player stats 
+    public float Health = 200;
+    public float Mana = 100;
+    public float basicAttack = 50;
+    public float SpecialAttack = 100;
+    public float Defense = 50;
+    public float luck;
+    public float scaling;
+
      void Start()
     {
         controller = GetComponent<CharacterController>();
