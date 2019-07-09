f2ea51bf7f209645d4c3ebaccedc0dda51e19306
Hodge0699 <39035064+Hodge0699@users.noreply.github.com>
2019-07-09 18:24:30 +0100

    Exp
    exp script for the player has been added, as well  as a gold scirpt has been created

---
+++ SHFT Game Project/Assets/Scripts/Exp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exp : MonoBehaviour
+{
+    //players current level
+    public int Level = 1;
+    //players current exp amount 
+    public int currentExp = 0;
+    //amount of exp needed to level up 
+    public int ExpBase = 10;
+    //amount of exp left for level up
+    public int ExpLeft = 10;
+    //the amount of exp that is needed for the next level up will increase 
+    public float ExpModifier = 1.15f;
+
+
+    void Update()
+    {
+        //base exp given per monster killed 
+        // if(BasicMonster.Dead)
+        //{
+        // gainExp(3_;  
+        //}
+    }
+    public void GainExp(int e)
+    {
+        currentExp += e;
+        if (currentExp >= ExpLeft)
+        {
+            LvlUp();
+        }
+    }
+    void LvlUp()
+    {
+        currentExp -= ExpLeft;
+        Level++;
+        float t = Mathf.Pow(ExpModifier, Level);
+        ExpLeft = (int)Mathf.Floor(ExpBase * t);
+    }
+}
---
+++ SHFT Game Project/Assets/Scripts/Gold.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int GoldValue = 1;
+}
